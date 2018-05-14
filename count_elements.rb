eecbc48ec0c82e82a1d611398e50eb1a313f4b6e
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-14 20:33:30 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,34 @@
+require 'pry'
+#returns a hash of key/value pairs in which 
+#keys are the original string elements of the array 
+#values are a count of how many times they appeared in the array.
+
 def count_elements(array)
   # code goes here
+  
+  new_hash = {}
+  
+  array.each do |element|
+    
+    #if string is already a key in hash => incremenet the values
+    if new_hash.has_key?(element)
+      new_hash[element] += 1
+    
+    #if not present, add it as a key to the hash with a value of 1
+    else
+      new_hash[element] = 1
+    end
+  end
+  
+  
+  
+  
+  
+  
+  
+  # binding.pry
+  new_hash
 end
+
+
  
