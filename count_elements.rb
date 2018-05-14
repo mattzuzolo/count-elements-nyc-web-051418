d989627998218e3a0379c40cbcfae2a6daa691c6
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-14 20:26:29 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,5 +1,7 @@
 
-#returns a hash of key/value pairs in which keys are the original string elements of the array and their associated values are a count of how many times they appeared in the array.
+#returns a hash of key/value pairs in which 
+#keys are the original string elements of the array 
+#values are a count of how many times they appeared in the array.
 
 def count_elements(array)
   # code goes here
